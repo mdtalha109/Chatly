0cdd533ac5e900e8e62c630ff36b5eefbf037eab
mdtalha109 <mdtalha1041@gmail.com>
2023-11-22 01:23:45 +0530

    feat: added framer motion on message item

--- frontend/src/components/chats/ScrollableChat.js
+++ frontend/src/components/chats/ScrollableChat.js
@@ -3,49 +3,50 @@ import React from 'react'
 import ScrollableFeed from 'react-scrollable-feed'
 import { isLastMessage, isSameSender, isSameSenderMargin, isSameUser } from '../../config/chatLogics'
 import { ChatState } from '../../Context/chatProvider'
+import { motion } from "framer-motion";
 
-const ScrollableChat = ({messages}) => {
-    const {user} = ChatState()
-    return (
-        <ScrollableFeed>
+const ScrollableChat = ({ messages }) => {
+  const { user } = ChatState()
+  return (
+    <ScrollableFeed>
       {messages &&
         messages.map((m, i) => (
-          <div style={{ display: "flex" }} key={m._id}>
+          <div style={{ display: "flex" }} key={i}>
             {(isSameSender(messages, m, i, user._id) ||
               isLastMessage(messages, i, user._id)) && (
-              <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
-                <Avatar
-                  mt="7px"
-                  mr={1}
-                  size="sm"
-                  cursor="pointer"
-                  name={m.sender.name}
-                  src={m.sender.pic}
-                />
-              </Tooltip>
-            )}
-            <span
+                <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
+                  <Avatar
+                    mt="7px"
+                    mr={1}
+                    size="sm"
+                    cursor="pointer"
+                    name={m.sender.name}
+                    src={m.sender.pic}
+                  />
+                </Tooltip>
+              )}
+            <motion.div
               style={{
-                backgroundColor: `${
-                  m.sender._id === user._id ? "#0EA487" : "#2B3856"
-                }`,
-                color:"white",
+                backgroundColor: `${m.sender._id === user._id ? "#0EA487" : "#2B3856"
+                  }`,
+                color: "white",
                 marginLeft: isSameSenderMargin(messages, m, i, user._id),
                 marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
                 borderRadius: "5px",
                 padding: "5px 15px",
                 maxWidth: "75%",
-                
-                
-               
               }}
+              animate={{ opacity: 1 }}
+              initial={{ opacity: 0 }}
+              transition={{ ease: "easeOut", duration: 2 }}
+              // layoutId="underline"
             >
               {m.content}
-            </span>
+            </motion.div>
           </div>
         ))}
     </ScrollableFeed>
-    )
+  )
 }
 
 export default ScrollableChat
