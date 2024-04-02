04a9379ddb639b234dddbc29f592785b63879cfd
mdtalha109 <mdtalha1041@gmail.com>
2024-04-02 23:54:06 +0530

    added cloudinary base url

--- frontend/src/config/baseConfig.js
+++ frontend/src/config/baseConfig.js
@@ -1,4 +1,9 @@
 export const BaseConfig = {
     BASE_SERVER_URL: 'http://localhost:4000',
-    BASE_API_URL : 'http://localhost:4000/api'
+    BASE_API_URL : 'http://localhost:4000/api',
+
+    CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/talhapro321/image/upload'
+
+    // BASE_SERVER_URL: 'https://chatly-hcey.onrender.com/',
+    // BASE_API_URL : 'https://chatly-hcey.onrender.com/api'
 }
