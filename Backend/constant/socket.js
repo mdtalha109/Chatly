1c273f605a54904d89f8a02bab6ed23e8b9a7ebb
mdtalha109 <mdtalha1041@gmail.com>
2024-04-07 01:22:35 +0530

    chore(socket event) : changed socket event string to constant

---
+++ Backend/constant/socket.js
@@ -0,0 +1,12 @@
+const socketEvent = {
+    SETUP : 'SETUP',
+    CONNECTED : 'CONNECTED',
+    TYPING : 'TYPING',
+    STOP_TYPING : 'STOP_TYPING',
+    MESSAGE_RECIEVED : 'MESSAGE_RECIEVED',
+    JOIN_CHAT : 'JOIN_CHAT',
+    NEW_MESSAGE : 'NEW_MESSAGE'
+}
+
+module.exports = socketEvent
+
