568a0919cf2f2b4a3470d14cb740645d12d3ce45
mdtalha109 <mdtalha1041@gmail.com>
2024-05-01 00:08:49 +0530

    style: added outline on getting focus

--- frontend/src/components/ui/Button/index.js
+++ frontend/src/components/ui/Button/index.js
@@ -5,7 +5,7 @@ const Button = ({ children, onClick, className, type='submit', ...rest }) => {
         <button
             type={type}
             onClick={onClick}
-            className={`py-2 px-2 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold outline-none bg-[#3528a5] text-white hover:bg-blue-600 focus:outline-none  text-sm  ${className}`}
+            className={`py-2 px-2 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold outline-2 outline-offset-2 focus:outline-black bg-[#3528a5] text-white hover:bg-blue-600 focus:outline-none  text-sm  ${className}`}
             {...rest}
         >
         
