21d85ef26077a548e3e8445b6e8b58575c5ef031
mdtalha109 <mdtalha1041@gmail.com>
2024-05-01 00:11:56 +0530

    fix: added no chat meessage

--- frontend/src/components/chats/ChatList/index.js
+++ frontend/src/components/chats/ChatList/index.js
@@ -15,22 +15,25 @@ const ChatList = ({ fetchAgain }) => {
   return (
     <div className={`flex flex-col items-center w-full visible`}
       d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      
     >
       <div className='flex flex-col  bg-[#F8F8F8] w-full h-full overflow-hidden'>
         {chats ? (
-          <div>
+          chats.length === 0 ? (
+            <div className='flex justify-center items-center h-[100vh]'>
+                <h1>No chat available</h1>
+            </div>
+          )
+            : (
+              <div>
             {chats.map((chat) => (
-              <div className={`px-5 py-3 flex items-center gap-2 cursor-pointer ${selectedChat === chat ? "bg-[#e8e8e9]" : "#E8E8E8"} border-b-2`}
+              <div className={`px-5 py-3 flex items-center gap-4 cursor-pointer ${selectedChat === chat ? "bg-[#e8e8e9]" : "#E8E8E8"} border-b-2`} 
+                onKeyPress={() => setSelectedChat(chat)}
                 onClick={() => setSelectedChat(chat)}
                 key={chat._id}
+                tabindex={0}
               >
-                <Avatar
-                  mr={2}
-                  size="sm"
-                  cursor="pointer"
-                  name={getSender(loggedUser, chat.users)?.name}
-                  src={getSender(loggedUser, chat.users)?.pic}
-                />
+                <img class="h-10 w-10 rounded-full object-cover" src={getSender(loggedUser, chat.users)?.pic} alt={getSender(loggedUser, chat.users)?.name}/>
 
                 <div className='flex flex-col gap-2'>
                   <p className='font-bold'>
@@ -39,7 +42,6 @@ const ChatList = ({ fetchAgain }) => {
                       : chat.chatName
                     }
                   </p>
-
 
                   {chat.latestMessage && (
                     <p className='text-xs'>
@@ -50,12 +52,10 @@ const ChatList = ({ fetchAgain }) => {
                     </p>
                   )}
                 </div>
-
-                
-
               </div>
             ))}
           </div>
+            )
         ) : (
           <div className='flex flex-col gap-2'>
             <Skeleton height="45px" />
