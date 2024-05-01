acd8f2aa3e46eaaf00ff18ab1785603c8b8d29e5
mdtalha109 <mdtalha1041@gmail.com>
2024-05-01 16:35:36 +0530

    style: fix online status style

--- frontend/src/components/chats/chatHeader/index.js
+++ frontend/src/components/chats/chatHeader/index.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { IoIosArrowBack } from 'react-icons/io'
 import { getSender } from '../../../config/chatLogics'
 
-const ChatHeader = ({setSelectedChat, user, selectedChat, isUserActive}) => {
+const ChatHeader = ({ setSelectedChat, user, selectedChat, isUserActive }) => {
     return (
-        <div className='flex px-2 py-4 items-center gap-2'>
+        <div className='flex px-2 py-4 items-center justify-between'>
 
-            <div className='cursor-pointer'>
-                <IoIosArrowBack onClick={() => setSelectedChat("")} />
-            </div>
-            <section className='flex flex-col'>
-                <div className='font-bold'>{getSender(user, selectedChat.users).name}</div>
-                {isUserActive ? <div className='text-green-500'>Online</div> : ''}
-            </section>
-
-
+            <seaction className='flex items-center gap-2'>
+                <div className='cursor-pointer'>
+                    <IoIosArrowBack onClick={() => setSelectedChat("")} />
+                </div>
+                <div className='flex flex-col '>
+                    <div className={`font-bold ${isUserActive ? 'text-sm' : ''}  `}>{getSender(user, selectedChat.users).name}</div>
+                    {isUserActive ? <div className='text-green-500 text-xs duration-1000'>Online</div> : ''}
+                </div>
+            </seaction>
         </div>
     )
 }
