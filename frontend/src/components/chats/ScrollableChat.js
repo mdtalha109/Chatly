8359dc6772834bbc2426224a23f34b9bf792ea4f
mdtalha109 <mdtalha1041@gmail.com>
2024-05-01 16:34:04 +0530

    fix: message scrolling issue

--- frontend/src/components/chats/ScrollableChat.js
+++ frontend/src/components/chats/ScrollableChat.js
@@ -1,35 +1,28 @@
 import React, { useEffect, useRef } from 'react'
-import { isLastMessage, isSameSender, isSameSenderMargin, isSameUser } from '../../config/chatLogics'
 import { ChatState } from '../../Context/chatProvider'
-import { motion } from "framer-motion";
 import MessageItem from './MessageItem';
 
-const ScrollableChat = ({ messages, scrollHandler, cursorRef }) => {
-
+const ScrollableChat = ({ messages }) => {
   const { user } = ChatState()
   const scrollRef = useRef();
-  const parentRef = useRef()
-  const previousScrollPosition = useRef(0);
-
 
   useEffect(() => {
     scrollRef.current?.scrollIntoView();
   }, [messages]);
 
-
-
-
   return (
-    <div className='overflow-y-auto ' ref={parentRef}>
+    <div className='overflow-y-auto '>
       {messages &&
         messages.map((message, index) => (
           <>
             <MessageItem
+              scrollRef={scrollRef}
               messages={messages}
               message={message}
               index={index}
               user={user}
             />
+
           </>
         ))}
     </div>
