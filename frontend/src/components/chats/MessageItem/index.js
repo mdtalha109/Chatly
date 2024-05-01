abeb5875434e9f60f17e22f1f404ad43d620c7d3
mdtalha109 <mdtalha1041@gmail.com>
2024-05-01 16:16:45 +0530

    refactor: moved message ui to separate component

---
+++ frontend/src/components/chats/MessageItem/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { isLastMessage, isSameSender, isSameSenderMargin, isSameUser } from '../../../config/chatLogics'
+
+const MessageItem = ({ messages, message, index, user }) => {
+    return (
+        <div className='flex items-center ' key={index}>
+            {(isSameSender({ messages }, message, index, user._id) ||
+                isLastMessage(messages, index, user._id)) && (
+                    <img
+                        src={message.sender.pic}
+                        alt='pic'
+                        className={`rounded-full h-8 w-8 `}
+                    />
+
+                )}
+
+            <div
+                style={{
+                    backgroundColor: `${message.sender._id === user._id ? "#0EA487" : "#2B3856"
+                        }`,
+                    color: "white",
+                    marginLeft: isSameSenderMargin(messages, message, index, user._id),
+                    marginTop: isSameUser(messages, message, index, user._id) ? 3 : 10,
+                    borderRadius: "5px",
+                    padding: "5px 15px",
+                    maxWidth: "75%",
+                }}
+            >
+                {message.image ? <img src={message.image} alt={message.image} className=' h-[450px] object-contain w-max' /> : <></>}
+                {message.content}
+                
+            </div>
+        </div>
+    )
+}
+
+export default MessageItem
