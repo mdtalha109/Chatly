1ec70f85cdbb7c26819817d725e4cbf13d234927
mdtalha109 <mdtalha1041@gmail.com>
2024-05-01 15:09:32 +0530

    refactor: moved chat list item ui to separate component

--- frontend/src/components/chats/ChatList/index.js
+++ frontend/src/components/chats/ChatList/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
+
 import useChatList from './hooks/useChatList';
-import moment from 'moment';
-import { formatDateTime } from '../../../utils/time';
 import ChatListSkeleton from '../ChatListSkeleton';
+import ChatListItem from '../ChatListItem';
 
 const ChatList = ({ fetchAgain }) => {
 
@@ -10,7 +10,6 @@ const ChatList = ({ fetchAgain }) => {
     chats,
     setSelectedChat,
     selectedChat,
-    getSender,
     loggedUser
   } = useChatList(fetchAgain)
 
@@ -30,38 +29,12 @@ const ChatList = ({ fetchAgain }) => {
             : (
               <div>
                 {chats.map((chat) => (
-                  <div className={`px-5 py-3 flex items-center justify-between  cursor-pointer ${selectedChat === chat ? "bg-[#e8e8e9]" : "#E8E8E8"} border-b-2`}
-                    onKeyPress={() => setSelectedChat(chat)}
-                    onClick={() => setSelectedChat(chat)}
-                    key={chat._id}
-                    tabindex={0}
-                  >
-                    <section className='flex gap-4'>
-                      <img class="h-10 w-10 rounded-full object-cover" src={getSender(loggedUser, chat.users)?.pic} alt={getSender(loggedUser, chat.users)?.name} />
-
-                      <div className='flex flex-col gap-2'>
-                        <p className='font-bold'>
-                          {chat
-                            ? getSender(loggedUser, chat.users)?.name
-                            : chat.chatName
-                          }
-                        </p>
-
-                        {chat.latestMessage && (
-                          <p className='text-xs'>
-                            {chat?.latestMessage?.sender?.name} :
-                            {chat?.latestMessage?.content?.length > 50
-                              ? chat?.latestMessage?.content?.substring(0, 51) + "..."
-                              : chat?.latestMessage?.content}
-
-                          </p>
-                        )}
-                      </div>
-                    </section>
-
-
-                    <span className='text-xs'>{formatDateTime(moment(chat?.latestMessage?.createdAt).format('YYYY-MM-DD HH:mm:ss'))}</span>
-                  </div>
+                 <ChatListItem
+                    chat={chat}
+                    selectedChat={selectedChat}
+                    setSelectedChat={setSelectedChat}
+                    loggedUser={loggedUser}
+                  />
                 ))}
               </div>
             )
---
+++ frontend/src/components/chats/ChatListItem/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { getSender } from '../../../config/chatLogics'
+import { formatDateTime } from '../../../utils/time'
+import moment from 'moment'
+
+const ChatListItem = ({chat, setSelectedChat, loggedUser, selectedChat}) => {
+  return (
+    <div className={`px-5 py-3 flex items-center justify-between  cursor-pointer ${selectedChat === chat ? "bg-[#e8e8e9]" : "#E8E8E8"} border-b-2`}
+    onKeyPress={() => setSelectedChat(chat)}
+    onClick={() => setSelectedChat(chat)}
+    key={chat._id}
+    tabindex={0}
+  >
+    <section className='flex gap-4'>
+      <img class="h-10 w-10 rounded-full object-cover" src={getSender(loggedUser, chat.users)?.pic} alt={getSender(loggedUser, chat.users)?.name} />
+
+      <div className='flex flex-col gap-2'>
+        <p className='font-bold'>
+          {chat
+            ? getSender(loggedUser, chat.users)?.name
+            : chat.chatName
+          }
+        </p>
+
+        {chat.latestMessage && (
+          <p className='text-xs'>
+            {chat?.latestMessage?.sender?.name} :
+            {chat?.latestMessage?.content?.length > 50
+              ? chat?.latestMessage?.content?.substring(0, 51) + "..."
+              : chat?.latestMessage?.content}
+
+          </p>
+        )}
+      </div>
+    </section>
+    <span className='text-xs'>{formatDateTime(moment(chat?.latestMessage?.createdAt).format('YYYY-MM-DD HH:mm:ss'))}</span>
+  </div>
+  )
+}
+
+export default ChatListItem
