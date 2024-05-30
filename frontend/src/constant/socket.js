367e9aade17dd4325144e09bc698385e84939537
mdtalha109 <mdtalha1041@gmail.com>
2024-05-30 21:25:09 +0530

    refactor: constant string for socket event

---
+++ frontend/src/constant/socket.js
@@ -0,0 +1,13 @@
+export const socketEvent = {
+    SETUP : 'SETUP',
+    connection : 'connection',
+    CONNECTED : 'CONNECTED',
+    TYPING : 'TYPING',
+    STOP_TYPING : 'STOP_TYPING',
+    MESSAGE_RECIEVED : 'MESSAGE_RECIEVED',
+    JOIN_CHAT : 'JOIN_CHAT',
+    NEW_MESSAGE : 'NEW_MESSAGE',
+    UPDATE_USER_STATUS: 'UPDATE_USER_STATUS',
+    GET_USER_STATUS: 'GET_USER_STATUS'
+}
+
