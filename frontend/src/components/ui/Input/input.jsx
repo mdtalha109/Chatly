d0e789896a0e5972761ad037ceadebe553703976
mdtalha109 <mdtalha1041@gmail.com>
2026-03-09 15:46:56 +0530

    refactor(Input): enhance input component structure and add leftAddon support

--- frontend/src/components/ui/Input/input.jsx
+++ frontend/src/components/ui/Input/input.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { cn } from '../../../utils/cn';
 
-const Input = React.forwardRef(({ id, label, placeholder, value, onChange, className, style, type = 'text', ...rest }, ref) => {
+const Input = React.forwardRef(({
+  id,
+  label,
+  placeholder,
+  value,
+  onChange,
+  className,
+  leftAddon,
+  style,
+  filled,
+  bordered = true,
+  type = 'text',
+  ...rest
+}, ref) => {
   const inputStyle = {
     ...style
   };
@@ -8,16 +22,27 @@ const Input = React.forwardRef(({ id, label, placeholder, value, onChange, class
   return (
     <div className={`input ${className}`} style={inputStyle}>
       {label && <label className="block mb-2 text-xs" htmlFor={`${id}`}>{label}</label>}
-      <input
-        id={id}
-        ref={ref}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className={`py-3 px-4 bg-white block w-full border outline-none rounded-md text-sm border-blue-500 focus:border-2${className}`}
-        {...rest}
-      />
+
+      <div className={cn(`relative border flex items-center rounded-md`, filled ? 'bg-gray-100' : 'bg-transparent', bordered ? 'border' : 'border-0')}>
+
+
+        {leftAddon && (
+          <div className="shrink-0 pl-2">
+            {leftAddon}
+          </div>
+        )}
+
+        <input
+          id={id}
+          ref={ref}
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className={cn(`py-2 px-4 block w-full  outline-none rounded-md text-sm`, filled ? 'bg-gray-100' : 'bg-white')}
+          {...rest}
+        />
+      </div>
     </div>
   );
 });
