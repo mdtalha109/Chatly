22ae680ca39b6b2c253a477c81cb93e1d4c799e0
mdtalha109 <mdtalha1041@gmail.com>
2026-04-04 23:28:18 +0530

    feat(chat): enhance MessageText component to render multiline content

--- frontend/src/components/chats/MessageItem/MessageText.jsx
+++ frontend/src/components/chats/MessageItem/MessageText.jsx
@@ -1,8 +1,20 @@
 import React, { memo } from 'react'
 
-const MessageText = memo(({ content, isStreaming }) => (
-  <span className="">
-    {content || ''}  </span>
-));
+const MessageText = memo(({ content, isStreaming }) => {
+  if (!content) return null;
+
+  const lines = content.split('\n');
+  
+  return (
+    <span className="whitespace-pre-wrap break-words">
+      {lines.map((line, index) => (
+        <React.Fragment key={index}>
+          {line}
+          {index < lines.length - 1 && <br />}
+        </React.Fragment>
+      ))}
+    </span>
+  );
+});
 
 export default MessageText;
